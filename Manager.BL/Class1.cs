2949c6cbb7457f2a239d9e61ca78c53c7b1509fb
ramzybassem100@gmail.com <ramzybassem100@gmail.com>
2022-10-25 20:00:55 +0200

    Added Database

---
+++ Manager.BL/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Manager.BL
+{
+    public class Class1
+    {
+
+    }
+}
--- PasswordManagerServer/Program.cs
+++ PasswordManagerServer/Program.cs
@@ -1,3 +1,8 @@
+using Manager.DAL.Data.Context;
+using Manager.DAL.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+// DbContext Container
+var connection = builder.Configuration.GetConnectionString("PasswordManagerDb");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connection));
+//Adding Entity Framework
+builder.Services.AddIdentity<Employee, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 
