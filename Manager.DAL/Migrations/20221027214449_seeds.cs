2cc7b1eb47731a2a3f2c0b290cd0cf2b93e62014
ramzybassem100@gmail.com <ramzybassem100@gmail.com>
2022-10-28 00:01:37 +0200

    Seeding

--- Manager.DAL/Migrations/20221027214449_seeds.cs
+++ Manager.DAL/Migrations/20221027214449_seeds.cs
@@ -9,7 +9,7 @@ namespace Manager.DAL.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.InsertData(
-                    table: "Categories",
+                    table: "Employees",
                     columns: new[] { "Id", "FirstName", "LastName", "UserName", "PhoneNumber" , "PasswordHash" },
                     values: new object[] { "13bc8ffd-e44e-40f1-98f0-f9c656cc4700", "Charles", "Troy","Charles" ,"01273119777", "AQAAAAEAACcQAAAAEMyS4zQCrdIHle/7e6C792W/MD5ikB+6Prqv7OQu70jPhXkRFxeBH/l55uiTG6csMQ==" }
 
